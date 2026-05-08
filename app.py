1c3c1f492d7b96c92563d2b9d58ad67e6204d6f7
Anohej <jehonazenuni995@gmail.com>
2026-05-08 10:12:04 +0200

    hey guys

--- app.py
+++ app.py
@@ -18,20 +18,20 @@ def get_db_connection():
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        brukernavn = request.form["brukernavn"]
+        password_hashed = request.form["password_hashed"]
+        hashed_password = hashlib.sha256(password_hashed.encode()).hexdigest()
 
         db = get_db_connection()
         cursor = db.cursor()
         try:
             cursor.execute(
-                "INSERT INTO users (username, password) VALUES (%s, %s)",
-                (username, hashed_password)
+                "INSERT INTO bruker (brukernavn, password_hashed) VALUES (%s, %s)",
+                (brukernavn, hashed_password)
             )
             db.commit()
         except mysql.connector.IntegrityError:
-            return "username already exists"
+            return "brukernavn already exists"
         finally:
             cursor.close()
             db.close()
@@ -47,26 +47,26 @@ def login():
         return redirect(url_for('index'))
     
     if request.method == "POST":
-        username = request.form["username"]
+        brukernavn = request.form["brukernavn"]
         password = request.form["password"]
         hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
         db = get_db_connection()
         cursor = db.cursor(dictionary=True)
         cursor.execute(
-            "SELECT * FROM users WHERE username = %s AND password = %s",
-            (username, hashed_password)
+            "SELECT * FROM users WHERE brukernavn = %s AND password = %s",
+            (brukernavn, hashed_password)
         )
-        user = cursor.fetchone()
+        bruker = cursor.fetchone()
         cursor.close()
         db.close()
 
         if user:
-            session['user_id'] = user['id']
-            session['username'] = user['username']
+            session['user_id'] = bruker['id']
+            session['brukernavn'] = bruker['brukernavn']
             return redirect(url_for('index'))
         else:
-            return "invalid username or password"
+            return "invalid brukernavn or password"
 
     return render_template("login.html")
 
@@ -97,16 +97,70 @@ def index():
     cursor.close()
     db.close()
 
-    # sender tasks (liste) og username (string) til html
-    return render_template('index.html', tasks=tasks, username=session['username'])
+    # sender tasks (liste) og brukernavn (string) til html
+    return render_template('index.html', tasks=tasks, brukernavn=session['brukernavn'])
+
+
+#ny oppgave
+@app.route('/add', methods=['POST'])
+def add_task():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    
+    #henter data fra skjemaet
+    title = request.form.get('title')
+    description = request.form.get('description')
+
+    db = get_db_connection
+    cursor = db.cursor()
+
+
+    cursor.execute(
+        "INSERT INTO tasks (title, description) VALUES (%s, %s)", #insert legger inn en ny oppgave
+        (title, description)
+    )
+
+    db.commit()
+    cursor.close()
+    db.close()
+
+    return redirect(url_for('index'))
+
+
+@app.route('/done/<int_task_id>') # markerer opg som frdy, <int: task_id> betyr at task_id er et heltall (int)
+def mark_done(tasks_id):
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    
+    db = get_db_connection()
+    cursor = db.cursor()
 
 
 
+@app.roure('/delete/<int:task_id>')
+def delete_task(tasks_id):
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    
+    db = get_db_connection()
+    cursor = db.cursor()
 
+    cursor.execute(
+        "DELETE FROM tasks WHERE id = %s",
+        (tasks_id,)
+    )
 
-#@app.route('/add')
+    db.commit()
+    cursor.close()
+    db.close()
+
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
+
+
+
+
         
